5eb1134a61b78af73294ddb20b8dd2a33e4e8ea3
Jennifer Haynes <haynes.jennifer@outlook.com>
2019-06-02 17:19:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,13 +16,19 @@ function logWhisper (string) {
 
 function sayHiToGrandma (string) {
   if (string.toLowerCase() === string) {
-    return "I can't hear you!"
+    return 'I can\'t hear you!'
   }
   
 }
 
 function sayHiToGrandma (string) {
   if (string.toUpperCase() === string) {
-    return "YES INDEED!"
+    return 'YES INDEED!'
+  }
+}
+
+function sayHiToGrandma (string) {
+  if (string() === 'I love you, Grandma.') {
+    return 'I love you, too.'
   }
 }
